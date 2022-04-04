b069196c59b4ae9ec2984af6f3a1f94e76a418a5
Christopher Aloisio <chris.aloisio@gmail.com>
2022-04-04 14:54:13 -0400

    can someone tell me how to make the sequential group work? I don't get it lol

--- commands/climber/moveclimberstofullhangingposition.py
+++ commands/climber/moveclimberstofullhangingposition.py
@@ -1,10 +1,25 @@
-from commands2 import CommandBase
+from commands1 import WaitCommand
+from commands2 import CommandBase, SequentialCommandGroup
 from subsystems.climbers.leftclimbersubsystem import LeftClimber
 from subsystems.climbers.rightclimbersubsystem import RightClimber
 import constants
 
 
-class MoveLeftClimberToFullHangingPosition(CommandBase):
+class LeftClimberPivotVertical(CommandBase):
+    def __init__(self, climber: LeftClimber) -> None:
+        CommandBase.__init__(self)
+        self.setName(__class__.__name__)
+        self.climber = climber
+        self.addRequirements(climber)
+
+    def initialize(self) -> None:
+        self.climber.leftClimber.retractPiston()
+
+    def isFinished(self) -> bool:
+        return True
+
+
+class RetractLeftClimberToFullHangingPosition(CommandBase):
     def __init__(self, climber: LeftClimber) -> None:
         CommandBase.__init__(self)
         self.setName(__class__.__name__)
@@ -15,7 +30,6 @@ class MoveLeftClimberToFullHangingPosition(CommandBase):
         self.climber.leftClimber.deactivateBrake()
 
     def execute(self) -> None:
-        self.climber.leftClimber.retractPiston()
         self.climber.leftClimber.setClimberHangingExtension()
 
     def end(self, _interrupted: bool) -> None:
@@ -35,6 +49,20 @@ class MoveLeftClimberToFullHangingPosition(CommandBase):
         )
 
 
+class RightClimberPivotVertical(CommandBase):
+    def __init__(self, climber: RightClimber) -> None:
+        CommandBase.__init__(self)
+        self.setName(__class__.__name__)
+        self.climber = climber
+        self.addRequirements(climber)
+
+    def initialize(self) -> None:
+        self.climber.rightClimber.retractPiston()
+
+    def isFinished(self) -> bool:
+        return True
+
+
 class MoveRightClimberToFullHangingPosition(CommandBase):
     def __init__(self, climber: RightClimber) -> None:
         CommandBase.__init__(self)
@@ -46,7 +74,6 @@ class MoveRightClimberToFullHangingPosition(CommandBase):
         self.climber.rightClimber.deactivateBrake()
 
     def execute(self) -> None:
-        self.climber.rightClimber.retractPiston()
         self.climber.rightClimber.setClimberHangingExtension()
 
     def end(self, _interrupted: bool) -> None:
@@ -64,3 +91,13 @@ class MoveRightClimberToFullHangingPosition(CommandBase):
             )
             < constants.kClimberRetractionPositionThreshold
         )
+
+
+class MoveLeftClimberToFullHangingPosition(SequentialCommandGroup):
+    def __init__(self, climber: LeftClimber):
+        super().__init__(
+            LeftClimberPivotVertical(climber),
+            WaitCommand(2),
+            RetractLeftClimberToFullHangingPosition(climber),
+        )
+        self.setName(__class__.__name__)
