8985885f68738c42c1a427bdf81af6a235d8e571
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-10-07 19:09:05 -0400

    always use odometry for tracking the hub (vision gets baked into odometer)

--- commands/shooter/aimshootertotarget.py
+++ commands/shooter/aimshootertotarget.py
@@ -20,37 +20,26 @@ class AimShooterToTarget(CommandBase):
         if SmartDashboard.getBoolean(
             constants.kReadyToFireKey, False
         ):  # only start tracking when ready to fire
-            distance = SmartDashboard.getNumber(
-                constants.kTargetDistanceRelativeToRobotKeys.valueKey, 0
+            currentPose = Pose2d(
+                *SmartDashboard.getNumberArray(
+                    constants.kRobotPoseArrayKeys.valueKey, [0, 0, 0]
+                )
             )
+
+            staticDifference = Transform2d(
+                currentPose, constants.kSimDefaultTargetLocation
+            )
+            staticRotation = rotationFromTranslation(staticDifference.translation())
+
+            self.shooter.rotateTurret(
+                optimizeAngle(constants.kTurretRelativeForwardAngle, staticRotation)
+                + constants.kTurretOffsetFromRobotAngle
+            )
+
+            distance = staticDifference.translation().norm()
             hoodAngle = constants.kHoodMappingFunction(distance)
             wheelSpeed = constants.kShootingMappingFunction(
                 distance
             ) + SmartDashboard.getNumber(constants.kWheelSpeedTweakKey, 0)
             self.shooter.setHoodAngle(Rotation2d.fromDegrees(hoodAngle))
             self.shooter.setWheelSpeed(wheelSpeed)
-
-            if SmartDashboard.getBoolean(
-                constants.kTargetAngleRelativeToRobotKeys.validKey, False
-            ):  # if we have an angle, use the relative angle to adjust the turret for precision
-                angle = SmartDashboard.getNumber(
-                    constants.kTargetAngleRelativeToRobotKeys.valueKey, 0.0
-                )
-
-                self.shooter.trackTurret(angle)  # always track the turret
-            else:  # ...otherwise use odometry to estimate where the target SHOULD be
-                currentPose = Pose2d(
-                    *SmartDashboard.getNumberArray(
-                        constants.kRobotPoseArrayKeys.valueKey, [0, 0, 0]
-                    )
-                )
-
-                staticDifference = Transform2d(
-                    currentPose, constants.kSimDefaultTargetLocation
-                )
-                staticRotation = rotationFromTranslation(staticDifference.translation())
-
-                self.shooter.rotateTurret(
-                    optimizeAngle(constants.kTurretRelativeForwardAngle, staticRotation)
-                    + constants.kTurretOffsetFromRobotAngle
-                )
